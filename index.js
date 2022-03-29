322cab8c5cef8c6370cf783c3f4b1734ed83b49b
Nalin Dwivedi <nalin.dwivedi@berkeley.edu>
2022-03-29 12:00:46 -0700

    wrote code for everything but mongoDB stuff that will be covered in Tuesday's lecture

--- index.js
+++ index.js
@@ -6,15 +6,23 @@ const port = 8080;
 app.use("/api", router); // API Root url at: http://localhost:8080/api
 
 // root url
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
     res.json({ message : "Welcome to the APOD app." });
 });
 
-app.post("/api/add", (req, res) => {
-    const url = req.body.image_url;
+router.get("/favorite", function (req, res) {
+    res.json({ "result" : }); // send back a list of image properties
+});
+  
+router.post("/add", function (req, res) {
+    const url = req.body.url;
     const date = req.body.date;
+    // store the url and date in mongoDB database
+});
 
-    res.send("POST Request Successful.");
+router.delete("/delete", function (req, res) {
+    const date = req.body.date;
+    // find the image with this date in mongoDB database, delete it
 });
 
 app.listen(port, () => {
